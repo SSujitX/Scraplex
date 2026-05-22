80f3d40b757d454a833a02ad3506bd53687417f7
SSujitX <ssujitxx@gmail.com>
2026-05-22 12:37:33 +0600

    feat: implement seleniumbase engine with UC and CDP proxy support

---
+++ Scraplex/engines/seleniumbase/__init__.py
@@ -0,0 +1,4 @@
+from Scraplex.engines.seleniumbase.engine import SeleniumBaseEngine
+from Scraplex.engines.seleniumbase.proxy import SeleniumBaseProxy
+
+__all__ = ["SeleniumBaseEngine", "SeleniumBaseProxy"]
